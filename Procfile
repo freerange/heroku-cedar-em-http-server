84e22375983bea184845f1e71188f8f4e494beec
James Mead <james@floehopper.org>
2011-07-14 11:26:52 +0100

    First stab at seeing whether we can use an EventMachine server on Heroku Cedar.

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: bundle exec ruby lib/loop.rb
---
+++ lib/loop.rb
@@ -0,0 +1,14 @@
+require "eventmachine"
+
+module EchoServer
+  def receive_data(data)
+    send_data(data)
+  end
+end
+
+EventMachine::run do
+  host = "0.0.0.0"
+  port = 8080
+  EventMachine::start_server host, port, EchoServer
+  puts "Started EchoServer on #{host}:#{port}"
+end
